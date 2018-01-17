f9b2419729a4fbc60afef7fe07f3488a177bda3b
TarasGrynik <grynik25.03@gmail.com>
2018-01-17 15:09:15 +0200

    add 2 operating sustem to validate

--- HW_DOMtask3/main.js
+++ HW_DOMtask3/main.js
@@ -8,15 +8,20 @@ var platform = navigator.platform.toLowerCase();
 
 for (var key in navigatorMessageMap) {
     key = key.toLowerCase();
-    var version = navigatorMessageMap[key];
 
     if (platform.indexOf(key) >= 0) {
         if (navigator.platform === 'Win32') {
             var $name = document.querySelector('.download-message[data-version="windows"]');
             $name.classList.add('showMessage');
-        } else {
+        } else if (navigator.platform === 'macos') {
             var $macOS = document.querySelector('.download-message[data-version="macos"]');
             $macOS.classList.add('showMessage');
+        } else if (navigator.platform === 'macos') {
+            var $linux = document.querySelector('.download-message[data-version="linux"]');
+            $linux.classList.add('showMessage');
+        } else {
+            var $ubuntu = document.querySelector('.download-message[data-version="ubuntu"]');
+            $ubuntu.classList.add('showMessage');
         }
     }
 }
