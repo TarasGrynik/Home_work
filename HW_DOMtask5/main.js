856f2bfc03f6d84b0a58fa4fe34cf59856279758
TarasGrynik <grynik25.03@gmail.com>
2018-01-17 15:14:38 +0200

    fix bug with stopProp

--- HW_DOMtask5/main.js
+++ HW_DOMtask5/main.js
@@ -1,4 +1,23 @@
-var onClick = function (e) {
+var onClickDef = function (e) {
+    var $el = e.target;
+    e.preventDefault();
+
+    $el.style.backgroundColor = 'yellow';
+    alert('CLICK!');
+    $el.style.backgroundColor = '';
+};
+
+document.body.onclick = onClickDef;
+var box1 = document.getElementById('box1');
+var box2 = document.getElementById('box2');
+var box3 = document.getElementById('box3');
+box1.onclick = onClickDef;
+box2.onclick = onClickDef;
+box3.onclick = onClickDef;
+
+
+
+var onClickProp = function (e) {
     var $el = e.target;
     e.stopPropagation();
 
@@ -7,10 +26,10 @@ var onClick = function (e) {
     $el.style.backgroundColor = '';
 };
 
-document.body.onclick = onClick;
-var box1 = document.getElementById('box1');
-var box2 = document.getElementById('box2');
-var box3 = document.getElementById('box3');
-box1.onclick = onClick;
-box2.onclick = onClick;
-box3.onclick = onClick;
+document.body.onclick = onClickProp;
+var box4 = document.getElementById('box4');
+var box5 = document.getElementById('box5');
+var box6 = document.getElementById('box6');
+box4.onclick = onClickProp;
+box5.onclick = onClickProp;
+box6.onclick = onClickProp;
